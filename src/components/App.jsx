2175479bd8a1f38482f596339bfe4b190e169fd9
LucianoBudassi <lucianobudassi@hotmail.com>
2022-08-28 23:14:12 -0300

    cuarta entrega

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import ItemsCards from './Cards/itemsCards';
+import { useState } from 'react';
 
 
 
 const App = () => {
+
+    const [cont, setCont] = useState(0);
+
     return (
        <>
        <Navbar/>
+       <ItemsCards/>
+       <p>
+        
+        Cantidad: {cont}
+
+       </p>
+       <button className='btn btn-sumar' onClick={() => setCont(cont + 1)}>
+            +
+        </button>
+       <button className='btn btn-restar'onClick={() => setCont(cont - 1)}>
+            -
+        </button>
        </>
        
     )
